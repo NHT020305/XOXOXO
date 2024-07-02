fb84f70b020f4ec5e88d5609933ecf48046b8c95
Huy Tung <tungtun020305@gmail.com>
2024-07-02 12:45:30 +0700

    Update files

--- fun.js
+++ fun.js
@@ -14,21 +14,19 @@ const server = http.createServer(handleRequest);
 
 server.listen(3002);*/
 
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-const htmlElement = "<form><label>Your Name </label><input type='text'></form>"
+const htmlElement =
+  "<form action='/store-user' method='POST'><label>Your Name </label><input type='text'></form>";
 
-app.get('/currenttime', function(req, res) {
-    res.send('<h1>' + new Date().toISOString() + '</h1>')
-})
+app.get("/currenttime", function (req, res) {
+  res.send("<h1>" + new Date().toISOString() + "</h1>");
+});
 
-app.get('/', function(req, res) {
-    res.send(htmlElement)
-})
+app.get("/", function (req, res) {
+  res.send(htmlElement);
+});
 
-app.listen(3004)
-
-
-
+app.listen(3004);
