ccc98e3dc80d3074293b167dda38b487d2b4786b
Huy Tung <tungtun020305@gmail.com>
2024-07-08 11:58:53 +0700

    Update project folder

--- main.js
+++ main.js
@@ -1,50 +1,97 @@
-const fs = require('fs')
+const fs = require("fs");
 const path = require("path");
+
 
 const express = require("express");
 
+
+const uuid = require('uuid')
+
+
 const app = express();
 
-app.use(express.static('public'))
-app.use(express.urlencoded({extended: false}));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+
 
 app.get("/", function (req, res) {
-  const htmlFilePath = path.join(__dirname, "views", "index.html");
-  res.sendFile(htmlFilePath);
+  res.render("index");
 });
+
 
 app.get("/about", function (req, res) {
-  const htmlFilePath = path.join(__dirname, "views", "about.html");
-  res.sendFile(htmlFilePath);
+  res.render("about");
 });
+
 
 app.get("/game", function (req, res) {
-  const htmlFilePath = path.join(__dirname, "views", "game.html");
-  res.sendFile(htmlFilePath);
+  res.render("game");
 });
 
-app.get("/connect", function (req, res) {
-  const htmlFilePath = path.join(__dirname, "views", "connect.html");
-  res.sendFile(htmlFilePath);
-});
 
-app.post('/connect', function(req, res) {
+app.get("/explore/:id", function(req, res) {
 
-  const diary = req.body;
-  
-  const filePath = path.join(__dirname, 'data', 'diary.json');
+  const invitedPlayerId = req.params.id;
+
+  const filePath = path.join(__dirname, "data", "diary.json");
   const fileData = fs.readFileSync(filePath);
-  const existingDiaries = JSON.parse(fileData);
-  existingDiaries.push(diary);
+  const storedDiaries = JSON.parse(fileData);
 
-  fs.writeFileSync(filePath, JSON.stringify(existingDiaries))
+  for (const diary of storedDiaries) {
+    if (diary.pid === invitedPlayerId ) {
+      return res.render('invited-player', { diary: diary});
+    }
+  }
 
-  res.redirect('/confirm');
+  res.render('404')
 })
 
-app.get("/confirm", function (req, res) {
-  const htmlFilePath = path.join(__dirname, "views", "confirm.html");
-  res.sendFile(htmlFilePath);
+
+app.get("/explore", function (req, res) {
+  
+  const filePath = path.join(__dirname, "data", "diary.json");
+  const fileData = fs.readFileSync(filePath);
+  const storedDiaries = JSON.parse(fileData);
+
+  res.render("explore", {
+    numberOfMatches: storedDiaries.length,
+    diaries: storedDiaries,
+  });
 });
+
+
+app.get("/connect", function (req, res) {
+  res.render("connect");
+});
+
+
+app.post("/connect", function (req, res) {
+
+  const diary = req.body;
+  diary.pid = uuid.v4(); // generating random ID but unique
+
+  const filePath = path.join(__dirname, "data", "diary.json");
+  const fileData = fs.readFileSync(filePath);
+  const storedDiaries = JSON.parse(fileData);
+  storedDiaries.push(diary);
+
+  fs.writeFileSync(filePath, JSON.stringify(storedDiaries));
+
+  res.redirect("/confirm");
+});
+
+
+app.get("/confirm", function (req, res) {
+  res.render("confirm");
+});
+
+app.use(function(req, res) {
+  res.render('404');
+})
 
 app.listen(3000);
